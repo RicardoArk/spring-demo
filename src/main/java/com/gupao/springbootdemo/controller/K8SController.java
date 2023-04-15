b54b0d1f69eb20832a252b50f26c0ea2aaeeefd3
RicardoArk <1584796183@qq.com>
2023-04-15 18:09:27 +0800

    test webhooks

--- src/main/java/com/gupao/springbootdemo/controller/K8SController.java
+++ src/main/java/com/gupao/springbootdemo/controller/K8SController.java
@@ -3,7 +3,6 @@ package com.gupao.springbootdemo.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 public class K8SController {
 
