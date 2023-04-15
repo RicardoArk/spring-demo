a6209c4000ab0e055bb13e0f0b80eb5c190559ca
RicardoArk <1584796183@qq.com>
2023-04-15 22:32:04 +0800

    test webhooks

--- src/main/java/com/gupao/springbootdemo/controller/K8SController.java
+++ src/main/java/com/gupao/springbootdemo/controller/K8SController.java
@@ -8,6 +8,6 @@ public class K8SController {
 
     @RequestMapping("/k8s")
     public String k8s(){
-        return "hello K8s <br/>111222 ";
+        return "hello K8s <br/>111222 11.23 Jack";
     }
 }
